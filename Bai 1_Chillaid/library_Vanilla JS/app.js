3c305ba1b48d70d59fc325c871e57a5a17920e2f
tuanhoang <rammatuan@gmail.com>
2022-03-02 09:54:37 +0700

    add

---
+++ Bai 1_Chillaid/library_Vanilla JS/app.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.chart').easyPieChart({
+        //your options goes here
+        size:200,
+        lineWidth:9,
+        scaleLength:15,
+        animate:2500
+    });
+});
