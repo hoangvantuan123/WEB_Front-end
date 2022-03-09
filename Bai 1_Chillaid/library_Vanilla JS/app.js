82bd1b28882dc3422eefe1d1ee360db3be20cbab
tuanhoang <rammatuan@gmail.com>
2022-03-09 21:51:20 +0700

    add

--- Bai 1_Chillaid/library_Vanilla JS/app.js
+++ Bai 1_Chillaid/library_Vanilla JS/app.js
@@ -1,10 +1,12 @@
 $(function() {
     $('.chart').easyPieChart({
         //your options goes here
+        barColor:'#A197BD',
         size:200,
         lineWidth:9,
         scaleLength:15,
-        animate:2500
+        animate:2500,
+
 
     });
 });
